5028c6d9adf79b12f7d4a9e6144260678dd2134b
André Martins <>
2016-01-16 22:06:49 -0300

    Pre-configuração do projeto.

---
+++ src/schema/desing/SchemaDesing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schema.desing;
+
+import schema.desing.view.main;
+
+/**
+ *
+ * @author Andre
+ */
+public class SchemaDesing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new main().setVisible(true);
+        });
+    }
+    
+}
